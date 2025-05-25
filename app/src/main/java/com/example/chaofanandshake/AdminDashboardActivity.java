f63146718d80229c9ecac4c2e473a7dfd57e8cec
Kyle <kylegonzales585@gmail.com>
2025-05-25 23:01:52 +0800

    Updated

--- app/src/main/java/com/example/chaofanandshake/AdminDashboardActivity.java
+++ app/src/main/java/com/example/chaofanandshake/AdminDashboardActivity.java
@@ -1,12 +1,16 @@
 package com.example.chaofanandshake;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,41 +18,89 @@ import com.example.chaofanandshake.Adapter.OrderAdapter;
 import com.example.chaofanandshake.Adapter.UserAdapter;
 import com.example.chaofanandshake.Domain.Order;
 import com.example.chaofanandshake.Domain.User;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.List;
 
-public class AdminDashboardActivity extends AppCompatActivity {
+public class AdminDashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     private RecyclerView productRecyclerView;
-    private UserAdapter userAdapter;
-    private DatabaseHelper dbHelper;
-
     private RecyclerView ordersRecyclerView;
-    private OrderAdapter orderAdapter;
 
+    private UserAdapter userAdapter;
+    private OrderAdapter orderAdapter;
+    private DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_dashboard); // your XML filename
+        setContentView(R.layout.activity_admin_dashboard);
 
+        // Setup Toolbar and Drawer
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.sidebar_admin);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Initialize RecyclerViews
         productRecyclerView = findViewById(R.id.productRecyclerView);
         productRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        dbHelper = new DatabaseHelper(this);
-        List<User> userList = dbHelper.getAllUsers(); // You’ll define this next
-
-        userAdapter = new UserAdapter(this, userList, dbHelper);
-        productRecyclerView.setAdapter(userAdapter);
 
         ordersRecyclerView = findViewById(R.id.Orders);
         ordersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // DatabaseHelper and adapters
         dbHelper = new DatabaseHelper(this);
-        List<Order> orderList = dbHelper.getAllOrders();  // Make sure this method returns List<Order>
 
+        List<User> userList = dbHelper.getAllUsers();
+        userAdapter = new UserAdapter(this, userList, dbHelper);
+        productRecyclerView.setAdapter(userAdapter);
+
+        List<Order> orderList = dbHelper.getAllOrders();
         orderAdapter = new OrderAdapter(this, orderList);
         ordersRecyclerView.setAdapter(orderAdapter);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.dashboard) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        } else if (id == R.id.users) {
+            startActivity(new Intent(this, TermsandcondiActivity.class));
+        } else if (id == R.id.product) {
+            startActivity(new Intent(this, ContactActivity.class));
+        } else if (id == R.id.analytics) {
+            Toast.makeText(this, "You have been Logged Out", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
